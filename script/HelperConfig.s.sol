94adafa48a9c2e9d12888809c631bec95e8120f5
Aashim <thelastmanarmy37@gmail.com>
2024-11-26 22:52:07 +0545

    mock Anvil Automation

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -1,13 +1,21 @@
 // SPDX-License-Identifier: MIT
-import {Script} from "forge-std/Script.sol";
 pragma solidity >=0.8.0 <0.9.0;
 
+import {Script} from "forge-std/Script.sol";
+import {VRFCoordinatorV2_5Mock} from "@chainlink/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
 
 abstract contract CodeConstants {
+    //VRF Mock Values
+    uint96 public MOCK_BASE_FEE = 0.25 ether;
+    uint96 public MOCK_GAS_PRICE_LINK = 1e9;
+    //LINK / ETH price
+    int256 public MOCK_WEI_PER_UINT_LINK = 4e15;
+
     uint256 public constant EHT_SEPOLIA_CHAIN_ID = 11155111;
     uint256 public constant LOCAL_CHAIN_ID = 31337;
 }
-contract HelperConfig is CodeConstants,Script {
+
+contract HelperConfig is CodeConstants, Script {
     error HelperConfig__InvalidChainId();
 
     struct NetworkConfig {
@@ -24,24 +32,54 @@ contract HelperConfig is CodeConstants,Script {
     constructor() {
         networkConfig[EHT_SEPOLIA_CHAIN_ID] = getSepoliaConfig();
     }
-    function getConfigByChainId(uint256 chainId) public  returns (NetworkConfig memory) {
-        if(networkConfig[chainId].vrfCoordinator != address(0)){
+
+    function getConfigByChainId(
+        uint256 chainId
+    ) public returns (NetworkConfig memory) {
+        if (networkConfig[chainId].vrfCoordinator != address(0)) {
             return networkConfig[chainId];
-        }else if(chainId == LOCAL_CHAIN_ID){
-            //getOrCreateAnvilChainEthConfig()
-        }else{
+        } else if (chainId == LOCAL_CHAIN_ID) {
+            return getAnvilConfig();
+        } else {
             revert HelperConfig__InvalidChainId();
         }
     }
+
     function getSepoliaConfig() public pure returns (NetworkConfig memory) {
         return
             NetworkConfig({
                 entryFee: 0.01 ether,
-                interval: 30,//in second
+                interval: 30, //in second
                 vrfCoordinator: 0x9DdfaCa8183c41ad55329BdeeD9F6A8d53168B1B,
                 gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
                 callbackGasLimit: 500000,
                 subscriptionId: 0
             });
+    }
+
+    function getAnvilConfig() public returns (NetworkConfig memory) {
+        if (LocalNetworkConfig.vrfCoordinator != address(0)) {
+            return LocalNetworkConfig;
+        }
+        vm.startBroadcast();
+        VRFCoordinatorV2_5Mock vrfCoordinatorMock = new VRFCoordinatorV2_5Mock(
+            MOCK_BASE_FEE,
+            MOCK_GAS_PRICE_LINK,
+            MOCK_WEI_PER_UINT_LINK
+        );
+        vm.stopBroadcast();
+        LocalNetworkConfig = NetworkConfig({
+            entryFee: 0.01 ether,
+            interval: 30,
+            callbackGasLimit: 500000,
+            subscriptionId: 0,
+            vrfCoordinator: address(vrfCoordinatorMock),
+            //gasLane value doesn't matter
+            gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae
+        });
+        return LocalNetworkConfig;
+    }
+    function getConfig() public returns (NetworkConfig memory) {
+        return getConfigByChainId(block.chainid);
     }
 }
--- script/Raffle.s.sol
+++ script/Raffle.s.sol
@@ -3,17 +3,32 @@ pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
 import {Raffle} from "src/Raffle.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployRaffle is Script {
     Raffle public raffle;
+
     function setUp() public {}
+
     function run() public {
         vm.startBroadcast();
         // raffle = new Raffle(5e16);
         vm.stopBroadcast();
     }
-    function deployContract ()public returns(Raffle,HelperConfig){
 
+    function deployContract() public returns (Raffle, HelperConfig) {
+        HelperConfig helperConfig = new HelperConfig();
+        HelperConfig.NetworkConfig memory config = helperConfig.getConfig();
+        vm.startBroadcast();
+        raffle = new Raffle(
+            config.entryFee,
+            config.interval,
+            config.vrfCoordinator,
+            config.gasLane,
+            config.subscriptionId,
+            config.callbackGasLimit
+        );
+        vm.stopBroadcast();
     }
 }
-//              
+//
